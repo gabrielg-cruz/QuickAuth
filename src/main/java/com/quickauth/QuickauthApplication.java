10b78b2f0d2a1c0ba3c0ddea653439b43cd66ba3
Gabriel <gabrielg-cruz@hotmail.com>
2025-01-06 13:37:03 -0300

    initial commit

---
+++ src/main/java/com/quickauth/QuickauthApplication.java
@@ -0,0 +1,13 @@
+package com.quickauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickauth/QuickauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
